96b323fff8dcf1f4a997ac2cb7a1e7128b186dbd
lingfei263 <391987933@qq.com>
2017-06-22 09:37:17 +0800

    更新utils

--- app/src/main/java/cn/ffb/xrecyclerview/app/MainActivity.java
+++ app/src/main/java/cn/ffb/xrecyclerview/app/MainActivity.java
@@ -3,7 +3,6 @@ package cn.ffb.xrecyclerview.app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import cn.ffb.xrecyclerview.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- xrecyclerviewlib/src/main/java/cn/ffb/widget/xrecyclerview/XRecyclerListView.java
+++ xrecyclerviewlib/src/main/java/cn/ffb/widget/xrecyclerview/XRecyclerListView.java
@@ -7,6 +7,8 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import cn.ffb.utils.DisplayUtils;
+
 
 /**
  * Created by lingfei on 2016/4/12.
